92c9f856bac0b12f05a3668ef1397c764e185bbf
David0403 <48942146+Noboroto@users.noreply.github.com>
2021-03-18 12:25:08 +0700

    Add handle

--- PidgeotMailMVVM/App.xaml.cs
+++ PidgeotMailMVVM/App.xaml.cs
@@ -16,12 +16,19 @@ namespace PidgeotMail
 			if (e != null)
 				Logs.Write(e.Exception.ToString());
 			e.Handled = true;
+			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyHandler);
 		}
 		void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
 		{
 			var ex = e.ExceptionObject as Exception;
 			if (e != null)
 				Logs.Write(ex.ToString());
+		}
+
+		static void MyHandler(object sender, UnhandledExceptionEventArgs args)
+		{
+			Exception e = (Exception)args.ExceptionObject;
+			Logs.Add(e.ToString());
 		}
 
 		private void Application_Exit(object sender, ExitEventArgs e)
