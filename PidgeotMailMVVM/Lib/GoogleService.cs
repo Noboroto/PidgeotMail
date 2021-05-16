ff001b4958f7e23029a52f31b744fd602fe5cbe3
David0403 <48942146+Noboroto@users.noreply.github.com>
2021-05-16 20:53:57 +0700

    Complete V 2.0

--- PidgeotMailMVVM/Lib/GoogleService.cs
+++ PidgeotMailMVVM/Lib/GoogleService.cs
@@ -17,7 +17,7 @@ namespace PidgeotMail.Lib
 		private static readonly string TextCredential = "{\"installed\":{\"client_id\":\"155023122563-9qnq9022jb9189377rjt98k91s393pen.apps.googleusercontent.com\",\"project_id\":\"PidgeotMail\",\"auth_uri\":\"https://accounts.google.com/o/oauth2/auth\",\"token_uri\":\"https://oauth2.googleapis.com/token\",\"auth_provider_x509_cert_url\":\"https://www.googleapis.com/oauth2/v1/certs\",\"client_secret\":\"ztuBZE_qYRBEJbYSAgbPimu4\",\"redirect_uris\":[\"urn:ietf:wg:oauth:2.0:oob\",\"http://localhost\"]}}";
 		public static UserCredential Credential => credential;
 
-		private static string[] Scopes = {GmailService.Scope.GmailSend, GmailService.Scope.GmailReadonly, SheetsService.Scope.Spreadsheets, "https://mail.google.com/" };
+		private static string[] Scopes = {GmailService.Scope.GmailReadonly, SheetsService.Scope.Spreadsheets, "https://mail.google.com/" };
 
 		private static Stream GenerateStreamFromString(string s)
 		{
